53da9983668165c58a43fa8e8706dbf0856741fc
SeungGyu <phillipskl123@gmail.com>
2024-07-04 16:23:57 +0900

    [feat] Swagger 적용 및 설정 추가

---
+++ src/main/java/com/example/instagramclone/config/OpenApiConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.instagramclone.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    private static final String API_NAME = "instagram-api";
+    private static final String API_VERSION = "1.0,0";
+    private static final String API_DESCRIPTION = "Instagram API";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(API_NAME)
+                        .description(API_DESCRIPTION)
+                        .version(API_VERSION));
+    }
+}
